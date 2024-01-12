5a4eba2e261318a0006b4677888306be4a2b7693
Anthony Fort <ajf2195@columbia.edu>
2024-01-12 13:36:05 +0000

    Makes sidebar items inaccessible from keyboard when sidebar is collapsed.

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const sidebar = document.querySelector('.sidebar')
   const sidebarOpenButton = document.getElementById('sidebar-open-button')
   const sidebarCloseButton = document.getElementById('sidebar-close-button')
+  const siderbarItems = document.getElementsByClassName('sidebar-item')
 
   let isSidebarOpen = false
 
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     e.stopPropagation()
     this.setAttribute('aria-expanded', 'true')
     sidebarCloseButton.setAttribute('aria-expanded', 'true')
+    Array.from(siderbarItems).forEach(item => item.setAttribute('tabindex', '0'))
     sidebar.style.left = '0px'
     isSidebarOpen = true
   })
@@ -18,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     e.stopPropagation()
     this.setAttribute('aria-expanded', 'false')
     sidebarOpenButton.setAttribute('aria-expanded', 'false')
+    Array.from(siderbarItems).forEach(item => item.setAttribute('tabindex', '-1'))
     sidebar.style.left = '-300px'
     isSidebarOpen = false
   })
@@ -26,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (isSidebarOpen === true) {
       sidebarOpenButton.setAttribute('aria-expanded', 'false')
       sidebarCloseButton.setAttribute('aria-expanded', 'false')
+      Array.from(siderbarItems).forEach(item => item.setAttribute('tabindex', '-1'))
       sidebar.style.left = '-300px'
       isSidebarOpen = false
     } 
