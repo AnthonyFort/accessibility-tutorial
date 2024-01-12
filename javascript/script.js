7c4ff4407236b5136670996e7d44f14e5fa08ec1
Anthony Fort <ajf2195@columbia.edu>
2024-01-12 12:55:37 +0000

    Makes nav bar collapsible

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const menuButton = document.querySelector('#header > button')
-  const sidebar = document.querySelector('.sidebar')
 
-  menuButton.addEventListener('click', function() {
+  const sidebar = document.querySelector('.sidebar')
+  const sidebarOpenButton = document.getElementById('sidebar-open-button')
+  const sidebarCloseButton = document.getElementById('sidebar-close-button')
+
+  sidebarOpenButton.addEventListener('click', function() {
     const isExpanded = this.getAttribute('aria-expanded') === 'true'
     this.setAttribute('aria-expanded', !isExpanded)
+    sidebarCloseButton.setAttribute('aria-expanded', !isExpanded)
     sidebar.style.left = isExpanded ? '-300px' : '0px'
   })
+
+  sidebarCloseButton.addEventListener('click', function(){
+    const isExpanded = this.getAttribute('aria-expanded') === 'true'
+    this.setAttribute('aria-expanded', !isExpanded)
+    sidebarOpenButton.setAttribute('aria-expanded', !isExpanded)
+    sidebar.style.left = isExpanded ? '-300px' : '0px'
+  })
+
 })
